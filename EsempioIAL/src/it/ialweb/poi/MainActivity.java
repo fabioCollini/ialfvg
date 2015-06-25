6f7444c0c1f6650171dd36127e17ee8a97f800ad
Fabio Collini <fabio.collini@gmail.com>
2015-06-25 15:03:04 +0200

    Crashlytics config

--- EsempioIAL/src/it/ialweb/poi/MainActivity.java
+++ EsempioIAL/src/it/ialweb/poi/MainActivity.java
@@ -3,11 +3,14 @@ package it.ialweb.poi;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.crashlytics.android.Crashlytics;
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Crashlytics.start(this);
 		setContentView(R.layout.activity_main);
 	}
 }
